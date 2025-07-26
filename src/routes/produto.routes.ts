f75256eaea89f5edea50461db0d63c1a316619d0
Bernevs <berneves2003@gmail.com>
2025-07-26 19:06:22 -0300

    feat: :sparkles:

--- src/routes/produto.routes.ts
+++ src/routes/produto.routes.ts
@@ -8,5 +8,6 @@ produtoRouter.get("/", ProdutoController.getProduto);
 produtoRouter.get("/:id", ProdutoController.getProdutoById);
 produtoRouter.put("/:id", ProdutoController.updateProduto);
 produtoRouter.delete("/:id", ProdutoController.deleteProduto);
+produtoRouter.post("/pdf", ProdutoController.getPDF);
 
 export default produtoRouter;
